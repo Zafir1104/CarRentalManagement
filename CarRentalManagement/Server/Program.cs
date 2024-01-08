0e7b5d8f265543cfcaa2e2380a850516c3e674f5
Zafir Zaffrudin <zafir.zaffrudin.school@gmail.com>
2024-01-09 02:17:29 +0800

    Added REST API Endpoints and Unit of Work pattern

--- CarRentalManagement/Server/Program.cs
+++ CarRentalManagement/Server/Program.cs
@@ -4,55 +4,65 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using CarRentalManagement.Server.IRepository;
+using CarRentalManagement.Server.Repository;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
-builder.Services.AddAuthentication()
-    .AddIdentityServerJwt();
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseMigrationsEndPoint();
-    app.UseWebAssemblyDebugging();
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+		var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlServer(connectionString));
+		builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+		builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+			.AddEntityFrameworkStores<ApplicationDbContext>();
+
+		builder.Services.AddIdentityServer()
+			.AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+		builder.Services.AddAuthentication()
+			.AddIdentityServerJwt();
+
+		builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+		builder.Services.AddControllersWithViews();
+		builder.Services.AddRazorPages();
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseMigrationsEndPoint();
+			app.UseWebAssemblyDebugging();
+		}
+		else
+		{
+			app.UseExceptionHandler("/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseBlazorFrameworkFiles();
+		app.UseStaticFiles();
+
+		app.UseRouting();
+
+		app.UseIdentityServer();
+		app.UseAuthorization();
+
+
+		app.MapRazorPages();
+		app.MapControllers();
+		app.MapFallbackToFile("index.html");
+
+		app.Run();
+	}
 }
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseIdentityServer();
-app.UseAuthorization();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
